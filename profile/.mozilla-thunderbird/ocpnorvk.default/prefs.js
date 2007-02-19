6c01cc5ae3efaafdcd7355c2423d599011b0e270
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-02-19 17:39:36 +0000

    etcskel-sidux (0.7.35) unstable; urgency=low
      * cover the new iceweasel profiles.
      * get rid of obsoleted thunderbird preconfig.
    
     -- Stefan Lippers-Hollmann <s.l-h@gmx.de>  Mon, 19 Feb 2007 18:38:40 +0100

--- profile/.mozilla-thunderbird/ocpnorvk.default/prefs.js
+++
@@ -1,7 +0,0 @@
-user_pref("font.default", "sans-serif");
-user_pref("font.minimum-size.x-western", 10);
-user_pref("font.size.fixed.x-western", 13);
-user_pref("font.size.variable.x-western", 13);
-user_pref("network.protocol-handler.app.ftp", "/usr/bin/firefox");
-user_pref("network.protocol-handler.app.https", "/usr/bin/firefox");
-user_pref("network.protocol-handler.app.http", "/usr/bin/firefox");
